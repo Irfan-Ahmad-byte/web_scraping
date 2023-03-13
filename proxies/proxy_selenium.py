5ab178e522ec8d872b0046adef3fc44548939851
Irfan-Ahmad-byte <irfan.ahmad.mlka@gmail.com>
2023-03-13 11:47:18 +0500

    renamed proxies.py to get_proxies.py

--- proxies/proxy_selenium.py
+++ proxies/proxy_selenium.py
@@ -1,36 +1,7 @@
 import csv
 import random
 import time
-
 from selenium import webdriver
-from selenium.webdriver.common.proxy import Proxy, ProxyType
-
-
-def test_ip(ip_address):
-    """
-    Tests if a given IP address is accessible by opening a browser window and visiting https://www.whatismyip.com/
-
-    Args:
-        ip_address (str): The IP address to test.
-
-    Returns:
-        bool: True if the IP address is accessible, False otherwise.
-    """
-    try:
-        options = webdriver.ChromeOptions()
-        proxy = Proxy()
-        proxy.proxy_type = ProxyType.MANUAL
-        proxy.http_proxy = ip_address
-        proxy.ssl_proxy = ip_address
-        options.add_argument(f'--proxy-server={ip_address}')
-        driver = webdriver.Remote(command_executor='http://localhost:4444', options=options)
-        driver.get('https://www.whatismyip.com/')
-        time.sleep(5)
-        driver.quit()
-        return True
-    except Exception as e:
-        raise e
-
 
 def get_working_ip(proxies):
     """
@@ -53,66 +24,58 @@ def get_working_ip(proxies):
             print(f"IP address {proxy} is blocked. Trying another IP address...")
     return None
 
-
-def use_proxies(url, num_tries=3):
+def test_ip(ip_address):
     """
-    Visits a website using different proxy IPs until successful.
+    Tests if a given IP address is accessible by opening a browser window and visiting https://www.whatismyip.com/
 
     Args:
-        url (str): The URL to visit.
-        num_tries (int): The number of attempts to make before giving up. Default is 3.
+        ip_address (str): The IP address to test.
 
     Returns:
-        None
+        bool: True if the IP address is accessible, False otherwise.
     """
+    try:
+        options = webdriver.ChromeOptions()
+        proxy = f"{ip_address}"
+        options.add_argument(f"--proxy-server={proxy}")
+        driver = webdriver.Chrome(options=options)
+        driver.get('https://www.whatismyip.com/')
+        time.sleep(5)
+        ip = driver.find_element_by_xpath('//span[@class="green"]/strong').text
+        driver.quit()
+        return ip
+    except:
+        return None
+
+def main():
     # Read the proxies from the CSV file
     with open('selected_proxies.csv', 'r') as csvfile:
         reader = csv.reader(csvfile)
         proxies = [row[0] for row in reader]
+    
+    # Get a working IP address
+    ip_address = get_working_ip(proxies)
+    if ip_address is None:
+        print("All IP addresses are blocked. Aborting...")
+        return
 
-    num_attempts = 0
-    while num_attempts < num_tries:
-        ip_address = get_working_ip(proxies)
-        if ip_address is None:
-            print("All IP addresses are blocked. Aborting...")
-            return
+    # Set up the browser with the proxy
+    options = webdriver.ChromeOptions()
+    proxy = f"{ip_address}"
+    options.add_argument(f"--proxy-server={proxy}")
+    driver = webdriver.Chrome(options=options)
 
-        try:
-            options = webdriver.ChromeOptions()
-            proxy = Proxy()
-            proxy.proxy_type = ProxyType.MANUAL
-            proxy.http_proxy = ip_address
-            proxy.ssl_proxy = ip_address
-            options.add_argument(f'--proxy-server={ip_address}')
-            driver = webdriver.Remote(command_executor='http://localhost:4444', options=options)
-            driver.get(url)
-            print(f"Using IP address: {ip_address}")
-            time.sleep(120)  # 2 minutes
-            driver.quit()
-            print("Session closed successfully")
-            num_attempts = 0  # reset num_attempts on successful session
-        except:
-            print(f"Session failed with IP address {ip_address}. Trying again...")
-            num_attempts += 1
+    # Visit Fiverr and get the current IP
+    driver.get('https://www.fiverr.com/')
+    current_ip = test_ip(ip_address)
+    if current_ip is None:
+        print("Could not get current IP address. Aborting...")
+    else:
+        print(f"Current IP address: {current_ip}")
 
-    print("All attempts failed. Aborting...")
+    # Close the browser
+    driver.quit()
 
-
-# Define the main function
-def main():
-    # Set the URL of the website to interact with
-    url = 'https://fiverr.com'
-
-    # Set the number of tries before giving up
-    num_tries = 3
-
-    # Call the use_proxies function
-    use_proxies(url, num_tries)
-
-
-# Run the main function
 if __name__ == '__main__':
     main()
-    
-    
 
